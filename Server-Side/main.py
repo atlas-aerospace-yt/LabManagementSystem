fdeb62acf7f05d36bcf545567921c2f0431d7f0b
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2023-10-30 12:22:10 +0000

    Initial commit

---
+++ Server-Side/main.py
@@ -0,0 +1,21 @@
+"""
+This is the main file which runs the whole server-side program.
+
+This code imports all libraries (custom and PIP) and runs the
+main loop.
+
+Date: 30/10/2023
+Author: Alexander Armitage
+"""
+
+# TODO get all libraries
+from server_manager import ServerManager
+
+# This if statement makes sure that the program
+# is not run when someone accidentally imports this
+# file as a library.
+if __name__ == "__main__":
+
+    # TODO main loop
+    my_server = ServerManager()
+    my_server.start_admin_terminal()
---
+++ Server-Side/server_manager.py
@@ -0,0 +1,69 @@
+"""
+This file contains the ServerManager class.
+"""
+import threading
+
+GREETING = "Welcome to the admin terminal!\n"
+
+INVALID_INPUT = "Invalid input!\n"
+
+MENU = """The options for the admin terminal are:\n
+    \"exit\" - ends the server side program.
+    \"disconnect-\"(all or ip) - disconects a specific user or all.
+    \"get_info\" - to get information about the activity.
+    \"SQL-\"(command) - run an SQL command on the servers\n"""
+
+COMMAND_LIST = ["disconnect",
+                "get_info"]
+
+
+class ServerManager:
+    """
+    The server manager class handles all of the admin inputs
+    into the application and connects the connection manager
+    to the database manager.
+
+    TODO comment attributes
+    TODO comment methods
+    """
+
+    def __init__(self):
+        self.terminal_thread = threading.Thread(target=self.threaded_admin_input)
+        self.commands = []
+        self.running = True
+
+        # TODO add the other managers
+        #self.connection_manager = ConnectionManager()
+        #self.database_manager = DatabaseManager()
+
+    def start_admin_terminal(self):
+        """
+        This function triggers the admin terminal.
+        """
+        self.terminal_thread.start()
+
+    def threaded_admin_input(self):
+        """
+        This function threads the administrator input.
+        """
+        print(GREETING)
+        print(MENU)
+
+        command = ""
+        invalid = False
+
+        while command.lower() != "exit":
+            if invalid:
+                print(INVALID_INPUT)
+                print(MENU)
+            invalid = True
+            command = input(">>> ")
+            if command[:3].lower() == "sql":
+                print("Your command: " + command[4:])
+                invalid = False
+            else:
+                for cmd in COMMAND_LIST:
+                    if command.lower()[:len(cmd)] == cmd:
+                        print("Your command: " + command[len(cmd)+1:])
+                        invalid = False
+                        break
