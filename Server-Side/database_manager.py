4bd117121e1917ad27e0263ae75fe54bde87df89
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2023-10-31 22:15:06 +0000

    Update database_manager.py

--- Server-Side/database_manager.py
+++ Server-Side/database_manager.py
@@ -1,8 +1,42 @@
 """
 This file contains the DatabaseManager class.
+
+Documentation of sqlite3: https://docs.python.org/3/library/sqlite3.html
 """
 
 import sqlite3
+
+CREATE_SUPPLIER_TABLE = """CREATE TABLE IF NOT EXISTS SUPPLIER
+(
+SupplierID INTEGER PRIMARY KEY,
+Name VARCHAR(100),
+);"""
+
+CREATE_STOCK_TABLE = """CREATE TABLE IF NOT EXISTS STOCK
+(
+    
+);"""
+
+CREATE_BOOKED_STOCK_TABLE = """CREATE TABLE IF NOT EXISTS BOOKED_STOCK
+(
+    
+);"""
+
+CREATE_BOOKING_TABLE = """CREATE TABLE IF NOT EXISTS BOOKING
+(
+    
+);"""
+
+CREATE_USERS_TABLE = """CREATE TABLE IF NOT EXISTS USERS
+(
+    
+);"""
+
+CREATE_LABS_TABLE = """CREATE TABLE IF NOT EXISTS LABS
+(
+    
+);"""
+
 
 class DatabaseManager:
     """
@@ -27,3 +61,12 @@ class DatabaseManager:
 
         self.cursor.execute(command)
         self.cursor.commit()
+        return self.cursor.fetchall
+
+    def end_connection(self):
+        """
+        This closes the connection between the server and the database.
+        """
+
+        self.cursor.close()
+        self.connection.close()
