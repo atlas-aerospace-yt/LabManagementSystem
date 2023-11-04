dd66beb620d7bd0ab38b62ca07227cb7f332ebf0
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2023-11-04 13:03:34 +0000

    Sending and receiving data
    Try and except prevents errors when a client closes improperly.

--- Client-Side/connection_manager.py
+++ Client-Side/connection_manager.py
@@ -1,12 +1,15 @@
 """
 This file contains the ConnectionManager class for the client
 side.
+
+TODO Commenting
+TODO Connect with main.py
 """
 
 import socket
 
-PORT = 6556
-IP_ADDRESS = "127.0.0.1"
+PORT = 6556 # a random port
+IP_ADDRESS = "127.0.0.1"# local host for testing
 
 class ConnectionManager:
     """
@@ -18,8 +21,38 @@ class ConnectionManager:
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect((IP_ADDRESS, PORT))
 
-# Check if this file is being run
-if __name__ == "__main__":
-    my_connection = ConnectionManager()
-    while True:
+    def connection_loop(self):
+        """
+        Loops the connection on a thread.
+        """
+        while True:
+            msg = input("Enter message: ")
+            self.client.send(msg.encode("UTF-8")[:1024])
+
+            response = self.client.recv(1024)
+            response = response.decode("utf-8")
+            
+            if response.lower() == "closed":
+                break
+
+            print(f"Received: {response}")
+        
+        self.client.close()
+        print("Connection to server closed")
+
+    def send_data(self, data):
+        """
+        This function sends data to the client side such as SQL outputs.
+        
+        Args:
+            data(str): the data that is to be sent to the client side
+        """
         pass
+
+
+# Check if this file is being run this loop is inspired by:
+# https://www.datacamp.com/tutorial/a-complete-guide-to-socket-programming-in-python
+if __name__ == "__main__":
+
+    my_connection = ConnectionManager()
+    my_connection.connection_loop()
--- Server-Side/connection_manager.py
+++ Server-Side/connection_manager.py
@@ -3,12 +3,16 @@ This file contains the ConnectionManager class
 
 The code is inspired by:
 https://www.datacamp.com/tutorial/a-complete-guide-to-socket-programming-in-python
+
+TODO Commenting
+TODO Connect with main.py
 """
 
+import time
 import socket
 import threading
 
-PORT = 6556
+PORT = 6556 # a random port
 IP_ADDRESS = "127.0.0.1" # local host for testing
 
 class Connection:
@@ -20,7 +24,43 @@ class Connection:
     def __init__(self, client_socket, client_address):
         self.socket = client_socket
         self.address = client_address
+        self.disconnected = False
         self.data = []
+        
+        self.connection = threading.Thread(target=self.connection_loop)
+        self.connection.start()
+
+    def connection_loop(self):
+        """
+        Loops the connection on a thread.
+        """
+        while True:
+            try:
+                request = self.socket.recv(1024)
+                request = request.decode("utf-8")
+            except ConnectionResetError:
+                break
+
+            if request.lower() == "close":
+                self.socket.send("closed".encode("utf-8"))
+                break
+
+            print(f"Received: {request}")
+            
+            response = "accepted".encode("utf-8")
+            self.socket.send(response)
+            time.sleep(2)
+
+        self.disconnected = True
+
+    def send_data(self, data):
+        """
+        This function sends data to the client side such as SQL outputs.
+        
+        Args:
+            data(str): the data that is to be sent to the client side
+        """
+        pass
 
 
 class ConnectionManager:
@@ -36,10 +76,10 @@ class ConnectionManager:
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((IP_ADDRESS, PORT))
 
-        self.get_connections = threading.Thread(target=self.threaded_get_connection())
+        self.get_connections = threading.Thread(target=self.get_connection)
         self.get_connections.start()
 
-    def threaded_get_connection(self):
+    def get_connection(self):
         """
         Loops waiting for connections to add them to the connections list.
         """
@@ -48,10 +88,10 @@ class ConnectionManager:
             self.server.listen(0)
 
             client_socket, client_address = self.server.accept()
-            self.connections.append(Connection(client_socket, client_address))
             print(f"Added a new connection to: {client_address[0]}:{client_address[1]}")
+            self.connections.append(Connection(client_socket, client_address))
 
-    def get_new_data(self) -> list:
+    def get_all_new_data(self) -> list:
         """
         Gets the commands which have been sent from the connections
 
@@ -62,5 +102,9 @@ class ConnectionManager:
 
 # Check if this file is being run
 if __name__ == "__main__":
-    my_connection = ConnectionManager()
-
+    my_server = ConnectionManager()
+    while True:
+        for item in my_server.connections:
+            if item.disconnected == True:
+                del my_server.connections[my_server.connections.index(item)]
+                print(f"Removed: {item.address[0]}:{item.address[1]}")
