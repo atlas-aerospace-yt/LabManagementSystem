fdf1c721075adbe4720748dbb78703d7712c7e06
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2023-11-05 22:44:49 +0000

    Runs a GUI now

--- Server-Side/main.py
+++ Server-Side/main.py
@@ -1,25 +1,28 @@
 """
 This is the main file which runs the whole server-side program.
 
-This code imports all libraries (custom and PIP) and runs the
-main loop.
+This code runs the main user interface for the server side.
 
 Date: 30/10/2023
 Author: Alexander Armitage
 """
 
-# TODO get all libraries
-from server_manager import ServerManager
+import sys
+from PyQt5 import QtWidgets as qtw
+
+from user_interface import UserInterface
 
 # This if statement makes sure that the program
 # is not run when someone accidentally imports this
 # file as a library.
 if __name__ == "__main__":
 
-    # TODO main loop
-    my_server = ServerManager()
-    my_server.start_admin_terminal()
-    my_server.main_loop()
+    app = qtw.QApplication(sys.argv)
 
-    # TODO At the end of the main loop
-    my_server.database_manager.end_connection()
+    server_side_ui = UserInterface()
+    server_side_ui.show()
+
+    app.exec_()
+
+    server_side_ui.end()
+    sys.exit()
