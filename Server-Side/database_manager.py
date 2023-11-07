d6f19f313201dbb8b12348d24e06a199f93f83ec
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2023-11-07 21:41:33 +0000

    Create sql_definitions

--- Server-Side/database_manager.py
+++ Server-Side/database_manager.py
@@ -5,62 +5,7 @@ Documentation of sqlite3: https://docs.python.org/3/library/sqlite3.html
 """
 
 import sqlite3
-
-CREATE_SUPPLIER_TABLE = """CREATE TABLE IF NOT EXISTS SUPPLIER
-(
-SupplierID INTEGER NOT NULL PRIMARY KEY,
-Name VARCHAR(100) NOT NULL,
-Phone VARCHAR(11),
-Email VARCHAR(254)
-);"""
-
-CREATE_STOCK_TABLE = """CREATE TABLE IF NOT EXISTS STOCK
-(
-StockID INTEGER NOT NULL PRIMARY KEY,
-Name VARCHAR(100) NOT NULL,
-Amount INTEGER NOT NULL,
-Price INTEGER NOT NULL,
-Website TEXT,
-SupplierID INTEGER,
-FOREIGN KEY(SupplierID) REFERENCES SUPPLIER(SupplierID)
-);"""
-
-CREATE_BOOKED_STOCK_TABLE = """CREATE TABLE IF NOT EXISTS BOOKED_STOCK
-(
-BookingID INTEGER,
-StockID INTEGER,
-Amount INTEGER NOT NULL,
-FOREIGN KEY(StockID) REFERENCES STOCK(StockID),
-FOREIGN KEY(BookingID) REFERENCES BOOKINGS(BookingID)
-);"""
-
-CREATE_BOOKINGS_TABLE = """CREATE TABLE IF NOT EXISTS BOOKINGS
-(
-BookingID INTEGER NOT NULL PRIMARY KEY,
-UserID INTEGER,
-LabID INTEGER,
-Date DATE NOT NULL,
-Time TIME NOT NULL,
-FOREIGN KEY(UserID) REFERENCES USERS(UserID),
-FOREIGN KEY(LabID) REFERENCES USERS(UserID)
-);"""
-
-CREATE_USERS_TABLE = """CREATE TABLE IF NOT EXISTS USERS
-(
-UserID INTEGER NOT NULL PRIMARY KEY,
-Name VARCHAR(100) NOT NULL,
-Email VARCHAR(254) NOT NULL,
-PasswordHash VARCHAR(255) NOT NULL,
-Priority INTEGER NOT NULL
-);"""
-
-CREATE_LABS_TABLE = """CREATE TABLE IF NOT EXISTS LABS
-(
-LabID INTEGER PRIMARY KEY,
-Name VARCHAR(100) NOT NULL,
-Location TEXT
-);"""
-
+import Definitions.sql_definitions as sql
 
 class DatabaseManager:
     """
@@ -73,12 +18,12 @@ class DatabaseManager:
         self.cursor = self.connection.cursor()
 
         # Create the database tables if they do not already exist
-        self.cursor.execute(CREATE_LABS_TABLE)
-        self.cursor.execute(CREATE_USERS_TABLE)
-        self.cursor.execute(CREATE_SUPPLIER_TABLE)
-        self.cursor.execute(CREATE_STOCK_TABLE)
-        self.cursor.execute(CREATE_BOOKINGS_TABLE)
-        self.cursor.execute(CREATE_BOOKED_STOCK_TABLE)
+        self.cursor.execute(sql.CREATE_LABS_TABLE)
+        self.cursor.execute(sql.CREATE_USERS_TABLE)
+        self.cursor.execute(sql.CREATE_SUPPLIER_TABLE)
+        self.cursor.execute(sql.CREATE_STOCK_TABLE)
+        self.cursor.execute(sql.CREATE_BOOKINGS_TABLE)
+        self.cursor.execute(sql.CREATE_BOOKED_STOCK_TABLE)
 
     def send_command(self, command:str)-> str:
         """
