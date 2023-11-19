9c58e6650922e30c82b08ed34dd43beef58ef22b
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2023-11-19 14:52:42 +0000

    Rename

---
+++ Client-Side/Definitions/gui_definitions.py
@@ -0,0 +1,48 @@
+"""
+This file holds the definitions for all the GUIs.
+"""
+
+# Graphing definitions
+MSG_TITLE = "<h1><p style=\"color:#fc7303\">Graph values (x, y)</p></h1>"
+MSG_BEGINNING = "<p style=\"color:#fc7303\">>>> <font color=\"#000000\">"
+MSG_ENDING = "</P>"
+
+# Main Window definitions
+DAYS_OF_THE_WEEK = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+TIME_SLOTS = ["0:00",
+              "1:00",
+              "2:00",
+              "3:00",
+              "4:00",
+              "5:00",
+              "6:00",
+              "7:00",
+              "8:00",
+              "9:00",
+              "10:00",
+              "11:00",
+              "12:00",
+              "13:00",
+              "14:00",
+              "15:00",
+              "16:00",
+              "17:00",
+              "18:00",
+              "19:00",
+              "20:00",
+              "21:00",
+              "22:00",
+              "23:00",
+              "24:00"]
+
+NUM_OF_ROWS = 25
+NUM_OF_COLS = 8
+
+TITLE_CSS = """
+*{
+    text-align: center;
+    color: #fc7303;
+    font-weight: bold;
+    padding-left: 5px;
+    padding-top: 5px;
+}"""
