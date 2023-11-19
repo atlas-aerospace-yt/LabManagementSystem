5416dc46fb95fa835dc50e77a3f15d807dbe4f0f
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2023-11-19 14:52:28 +0000

    Delete

--- Client-Side/Definitions/graph_definitions.py
+++
@@ -1,7 +0,0 @@
-"""
-This file holds the definitions for the graphing UI.
-"""
-
-MSG_TITLE = "<h1><p style=\"color:#fc7303\">Graph values (x, y)</p></h1>"
-MSG_BEGINNING = "<p style=\"color:#fc7303\">>>> <font color=\"#000000\">"
-MSG_ENDING = "</P>"
