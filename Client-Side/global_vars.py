db8ae3c7a2aaa5ceb517d20a8b4652fac921e02e
Atlas Aerospace <79483525+atlas-aerospace-yt@users.noreply.github.com>
2023-12-16 14:28:50 +0000

    Public method

--- Client-Side/global_vars.py
+++ Client-Side/global_vars.py
@@ -14,3 +14,26 @@ USER_EMAIL = None
 LOGGED_IN = False
 
 PRIORITY = -1
+
+def get_first_id(query:str) -> int:
+    """
+    Get the first available Primary key that can be used to insert a new record.
+
+    Args:
+        query(str): The query that selects the primary keys that already exist.
+
+    Returns:
+        int: the first available booking ID.
+    """
+    booking_ids = CONNECTION_MANAGER.send_command(query)
+    ordered_ids = sorted([int(i[0]) for i in booking_ids])
+    used_id = ordered_ids[0]
+
+    if used_id == 0:
+        for booking_id in ordered_ids[1:]:
+            if used_id+1 != booking_id:
+                return used_id+1
+            used_id += 1
+    else:
+        return 0
+    return used_id+1
